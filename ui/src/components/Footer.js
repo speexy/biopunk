3e9293a7e740dd8703fe5915a8b96ecc1d030c94
speexy <jennifer.meyer@posteo.de>
2022-02-25 12:35:04 +0100

    feat: bmbf logo in footer

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Map from '../../static/media/map3.png';
+import BMBFLogo from '../../static/media/BMBF_gefördert vom_deutsch.jpg';
 import styles from './footer.module.css';
 
 const Footer = () => (
     <div className={styles.footer}>
         <a href="https://www.openstreetmap.de/karte.html?zoom=15&lat=52.38885&lon=13.07688&layers=B000" target="_black" style={{ backgroundImage: `url(${Map})` }}></a>
         <div>
-            <p>Wissenschaftsladen Potsdam e.V.</p>
-            <p>Friedrich-Engels-Straße 22</p>
-            <p>Haus1 freiLand Potsdam (machBar @Haus5) </p>
-            <p>Germany - 14469 Potsdam </p>
-            <p>Banc Account: DE61 1203 0000 1020 5274 02</p>
-            <p>Contact: info@wissenschaftsladen-potsdam.de</p>
-            <Link to={'/legal'} > >> Imprint & Privacy </Link>
+          <div className={styles.adress}>
+              <p>Wissenschaftsladen Potsdam e.V.</p>
+              <p>Friedrich-Engels-Straße 22</p>
+              <p>Haus1 freiLand Potsdam (machBar @Haus5) </p>
+              <p>Germany - 14469 Potsdam </p>
+              <p>Banc Account: DE61 1203 0000 1020 5274 02</p>
+              <p>Contact: info@wissenschaftsladen-potsdam.de</p>
+              <Link to={'/legal'} > >> Imprint & Privacy </Link>
+          </div>
+          <div className={styles.bmbf}>
+            <img src={BMBFLogo}/>
+            <p>Die Verantwortung für den Inhalt dieser Veröffentlichung liegt beim Autor.</p>
+          </div>
         </div>
     </div>
 );
